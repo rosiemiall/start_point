056fc2e3ec7a337aba7154cdc124895cfe20e585
Rosie Miall <rosiemiall38@gmail.com>
2023-10-10 16:21:16 +0100

    part 2b computer done

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -2,10 +2,12 @@ public class Computer {
 
     private int storage;
     private String printer;
+    private boolean isPrinterConnected;
 
     public Computer(int inputStorage){
         this.storage = inputStorage;
         this.printer = "no printer";
+        this.isPrinterConnected = false;
     }
 
     public int getStorage(){
@@ -22,13 +24,19 @@ public class Computer {
     
     public void setPrinter(String newPrinter){
         this.printer= newPrinter;
-    }
-
-
+        System.out.println(newPrinter);
+        this.isPrinterConnected = true;    }
 
     public int addStorage(int extraStorage){
         this.storage += extraStorage;
         return this.storage ;
     }
-
+    
+    public void printMessage(String message){
+        
+        if (isPrinterConnected){
+            System.out.println(message);
+        }
+    }
+    
 }
